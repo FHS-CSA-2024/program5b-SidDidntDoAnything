bd739e042c1d727047753100d700581759bd57ca
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-09-11 23:56:39 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -5,5 +5,17 @@ public CarTester {
     
     Car car = new Car();
   }
-  public void AddCar(){
+ 
+  public Car AddCar() {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Please enter the car's name: ");
+    String name = sc.nextString();
+    System.out.println("\nPlease enter cars miles: ");
+    double miles = sc.nextDouble();
+    System.out.print("\nPlease enter cars gallons: ");
+    double gallons = sc.nextDouble();
+    Car car = new Car(name, miles, gallons);
+    return car;
+  }
     

7eb41e2a9f0f6f427751ac64b6570ff941ca1cc7
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-09-12 00:50:56 -0700

    Update CarTester.java

--- CarTester.java
+++ CarTester.java
@@ -1,23 +1,22 @@
-public CarTester {
+import java.util.Scanner;
+public class CarTester {
   public static void main(String[] args) {
+    for (int i = 0; i<2; i++) {
+    System.out.println();
     CarTester test = new CarTester();
-    test.AddCar();
-    car.calculateAverage();
-    car.toString();
-    
-    
-   
+    Car car = test.AddCar(); 
+    System.out.println("\n" + car.toString()); 
   }
- 
+  }
   public Car AddCar() {
-    Scanner sc = new Scanner(System.in);
+    Scanner sca = new Scanner(System.in);
     System.out.println("Please enter the car's name: ");
-    String name = sc.nextString();
+    String name = sca.nextLine();
     System.out.println("\nPlease enter cars miles: ");
-    double miles = sc.nextDouble();
+    double miles = sca.nextDouble();
     System.out.print("\nPlease enter cars gallons: ");
-    double gallons = sc.nextDouble();
+    double gallons = sca.nextDouble();
     Car car = new Car(name, miles, gallons);
     return car;
   }
-    
+}

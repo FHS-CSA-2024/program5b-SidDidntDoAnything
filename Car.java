4b82a39bfa45b6745d1f4c1926ac1da9c466b97b
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-09-12 00:51:06 -0700

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,40 +1,33 @@
 public class Car {
-private String carName = "";
-private double mileDriven = 0;
-private double gallonsUsed = 0;
-    
-  public Car () {
-    carName = "";
-    milesDriven = 0;
-    gallonsUsed = 0;
+    private String carName;
+    private double milesDriven;
+    private double gallonsUsed;
+     public Car () {
+        carName = "";
+        milesDriven = 0;
+        gallonsUsed = 0;
       }
-  
-  public Car (String name, double miles, double gallons) {
-    carName = name;
-    mileDriven = miles;
-    gallonsUsed = gallons;
-  }
-
-  public String getName(){
-    return carName;
-  }
-
-  public double getMiles() {
-    return milesDriven;
-  }
-
-  public double getGallons() {
-    return gallonsUsed;
-  }
-
-  public double calculateAverage() {
-    double avg = Math.round(mileDriven/gallonsUsed * 10.0) / 10.0;
-    return avg;
-  }
-
-  public String toString() {
-    String total = carName + " averaged " + milesDriven + " m/g";
-    return total;
-}
-}
-
+      public Car (String name, double miles, double gallons) {
+        carName = name;
+        milesDriven = miles;
+        gallonsUsed = gallons;
+      }
+      public String getName(){
+        return carName;
+      }
+      public double getMiles() {
+        return milesDriven;
+      }
+      public double getGallons() {
+        return gallonsUsed;
+      }
+      public double calculateAverage() {
+        double avg = Math.round(((double)milesDriven/gallonsUsed) * 10) / 10;
+        return avg;
+        
+      }
+      public String toString() {
+        String total = carName + " averaged " + milesDriven / gallonsUsed + " m/g";
+        return total;
+    }
+    }
